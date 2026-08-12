580c734edf103e5375e72db6ca2582acb617cc6c
Pedro Falcato <pedro.falcato@gmail.com>
2026-08-12 23:45:37 +0100

    onyx
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- src/backends.rs
+++ src/backends.rs
@@ -108,6 +108,7 @@ cfg_if! {
     } else if #[cfg(any(
         target_os = "android",
         target_os = "linux",
+        target_os = "onyx",
         target_os = "dragonfly",
         target_os = "freebsd",
         target_os = "hurd",
